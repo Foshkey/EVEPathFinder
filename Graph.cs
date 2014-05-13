8c0622a7f3f6b26408004366532c930acf5fd5f0
Foshkey <foshkey@gmail.com>
2014-05-13 15:29:11 -0500

    Updated Header

--- Graph.cs
+++ Graph.cs
@@ -7,7 +7,7 @@
  *   /_/   |_____//_____/ /_/ /_//_/ \_\/_____//_____/       *
  *                                                           *
  * This source file is developed by Joshua J. Nelson         *
- * Copyright ©2013 Foshkey Productions, foshkey@gmail.com    *
+ * Contact: foshkey@gmail.com                                *
  *                                                           *
  * Permission is hereby granted, free of charge, to any      *
  * person obtaining a copy of this software and associated   *
@@ -19,9 +19,9 @@
  * is furnished to do so, subject to the following           *
  * conditions:                                               *
  *                                                           *
- * The above copyright notice and this permission notice     *
- * shall be included in all copies or substantial portions   *
- * of the Software.                                          *
+ * The above notice and this permission notice shall be      *
+ * included in all copies or substantial portions of the     *
+ * Software.                                                 *
  *                                                           *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY *
  * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO    *
